5e3fae8e2d00def418ed45e719ae54f0d171797c
MD <122848691+WorkCwp@users.noreply.github.com>
2023-11-06 22:45:48 -0500

    ♰⍣𝑪𝒓𝒊𝒘𝒊𝒍𝒐𝒑𝑩𝒐𝒕-𝑴𝑫࿐⛦

--- plugins/menu-menu.js
+++ plugins/menu-menu.js
@@ -3,7 +3,7 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
   if (usedPrefix == 'a' || usedPrefix == 'A') return;
   try {
       m.react('🍰')
-   // const pp = imagen4;
+   const pp = imagen4;
     // let vn = './media/menu.mp3'
     const d = new Date(new Date + 3600000);
     const locale = 'es';
