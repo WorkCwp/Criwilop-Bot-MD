5a94d9dd16c665c81dac12a790b5177a459a5626
MD <122848691+WorkCwp@users.noreply.github.com>
2023-11-06 19:45:07 -0500

    ♰⍣𝑪𝒓𝒊𝒘𝒊𝒍𝒐𝒑𝑩𝒐𝒕-𝑴𝑫࿐⛦

--- plugins/info-gruposofc.js
+++ plugins/info-gruposofc.js
@@ -4,7 +4,7 @@ const handler = async (m, {conn, usedPrefix}) => {
   const text = `*𝙷𝙾𝙻𝙰 𝚄𝚂𝚄𝙰𝚁𝙸𝙾 👋🏻, 𝚃𝙴 𝙸𝙽𝚅𝙸𝚃𝙾 𝙰 𝚄𝙽𝙸𝚁𝚃𝙴 𝙰 𝙻𝙾𝚂 𝙶𝚁𝚄𝙿𝙾𝚂 𝙾𝙵𝙸𝙲𝙸𝙰𝙻𝙴𝚂 𝙳𝙴 𝚃𝙷𝙴 𝚂𝙷𝙰𝙳𝙾𝚆 𝙱𝚁𝙾𝙺𝙴𝚁𝚂 - 𝚃𝙴𝙰𝙼 𝙿𝙰𝚁𝙰 𝙲𝙾𝙽𝚅𝙸𝚅𝙸𝚁 𝙲𝙾𝙽 𝙻𝙰 𝙲𝙾𝙼𝚄𝙽𝙸𝙳𝙰𝙳 :D*
 
 *➤ 𝙶𝚛𝚞𝚙𝚘 𝚘𝚏𝚒𝚌𝚒𝚊𝚕 𝚍𝚎𝚕 𝙱𝚘𝚝:*
-*1.-* https://chat.whatsapp.com/LjJbmdO0qSDEKgB60qivZj`.trim();
+*1.-* https://chat.whatsapp.com/IPBbr3nEmSkKzkJbWGtlDy`.trim();
   const buttonMessage= {
     'document': {url: `https://github.com/WorkCwp/Criwilop-Bot-MD`},
     'mimetype': `application/${document}`,
