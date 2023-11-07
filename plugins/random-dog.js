bc1526fa364085e515e9a82b5133ba31f48ab641
MD <122848691+WorkCwp@users.noreply.github.com>
2023-11-06 23:29:35 -0500

    ♰⍣𝑪𝒓𝒊𝒘𝒊𝒍𝒐𝒑𝑩𝒐𝒕-𝑴𝑫࿐⛦

--- plugins/random-dog.js
+++ plugins/random-dog.js
@@ -3,7 +3,7 @@ const handler = async (m, {conn, text}) => {
   try {
     const res = await fetch('https://api.thedogapi.com/v1/images/search');
     const img = await res.json();
-    const caption = `_©The Mystic - Bot_`.trim();
+    const caption = `_♰⍣𝑪𝒓𝒊𝒘𝒊𝒍𝒐𝒑𝑩𝒐𝒕-𝑴𝑫࿐⛦_`.trim();
     conn.sendFile(m.chat, img[0].url, 'dog.jpg', caption, m);
   } catch {
     throw '*Error!*';
