34ae114dbc3db8e043d045c10a4cec1fcdf31a9c
MD <122848691+WorkCwp@users.noreply.github.com>
2023-11-09 00:18:36 -0500

    ♰⍣𝑪𝒓𝒊𝒘𝒊𝒍𝒐𝒑𝑩𝒐𝒕-𝑴𝑫࿐⛦

--- plugins/Htmi.js
+++ plugins/Htmi.js
@@ -1,10 +1,10 @@
 import fetch from 'node-fetch';
 const handler = async (m, {conn, text}) => {
   try {
-    const res = await fetch('https://hitomi.la/reader/2722567.html');
+    const res = await fetch('');
     const img = await res.json();
     const caption = `_♰⍣𝑪𝒓𝒊𝒘𝒊𝒍𝒐𝒑𝑩𝒐𝒕-𝑴𝑫࿐⛦_`.trim();
-    conn.sendFile(m.chat, img[0].url, 'dog.jpg', caption, m);
+    conn.sendFile(m.chat, img[0].url, 'htmi.jpg', caption, m);
   } catch {
     throw '*Error!*';
   }
