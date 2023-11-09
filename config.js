17e8ce1b7f8dbe51491c67b74845333be7ba7b49
MD <122848691+WorkCwp@users.noreply.github.com>
2023-11-09 17:39:06 -0500

    ♰⍣𝑪𝒓𝒊𝒘𝒊𝒍𝒐𝒑𝑩𝒐𝒕-𝑴𝑫࿐⛦

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ import fetch from 'node-fetch';
 import axios from 'axios';
 import moment from 'moment-timezone';
 
-global.botnumber = "573003025991"
+global.botnumber = "573138207218"
 
 global.owner = [
   ['573244488043', '☘️', true],
