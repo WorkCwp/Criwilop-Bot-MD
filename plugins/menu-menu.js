10030f5ed5d5af1fcdd98597b2508d1755153e6e
MD <122848691+WorkCwp@users.noreply.github.com>
2023-11-08 20:56:49 -0500

    ♰⍣𝑪𝒓𝒊𝒘𝒊𝒍𝒐𝒑𝑩𝒐𝒕-𝑴𝑫࿐⛦

--- plugins/menu-menu.js
+++ plugins/menu-menu.js
@@ -506,10 +506,7 @@ try {
 await conn.sendMessage(m.chat, { image: CwpImg.getRandom(), gifPlayback: false, caption: menu, mentions: [m.sender, global.conn.user.jid] }, { quoted: fkontak }) 
 } catch (error) {
 try{
-await conn.sendFile(m.chat, imagen5, 'menu.jpg', menu, fkontak, false, { mentions: [m.sender, global.conn.user.jid] })
-} catch (error) {
-return 
-}}}} 
+}}}
 
   }
 };
