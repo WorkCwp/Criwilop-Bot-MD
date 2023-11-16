0481c40ac6b958433eabcc83a357ccffa6277b86
MD <122848691+WorkCwp@users.noreply.github.com>
2023-11-15 21:48:15 -0500

    ♰⍣𝑪𝒓𝒊𝒘𝒊𝒍𝒐𝒑𝑩𝒐𝒕-𝑴𝑫࿐⛦

--- plugins/gc-delete.js
+++ plugins/gc-delete.js
@@ -12,22 +12,8 @@ const handler = async (m, {conn, usedPrefix, command}) => {
 };
 handler.help = ['del', 'delete'];
 handler.tags = ['group'];
-handler.command = /^del(ete)?$/i;
+handler.command = /^x|X)?$/i;
 handler.group = true;
 handler.admin = true;
 handler.botAdmin = true;
 export default handler;
-
-/* let handler = function (m) {
-if (!m.quoted) throw false
-let { chat, fromMe, isBaileys } = m.quoted
-if (!fromMe) throw false
-if (!isBaileys) throw '*⚠️ 𝙴𝚂𝙴 𝙼𝙴𝙽𝚂𝙰𝙹𝙴 𝙽𝙾 𝙵𝚄𝙴 𝙴𝙽𝚅𝙸𝙰𝙳𝙾 𝙿𝙾𝚁 𝙼𝙸, 𝙽𝙾 𝙻𝙾 𝙿𝚄𝙴𝙳𝙾 𝙴𝙻𝙸𝙼𝙸𝙽𝙰𝚁*'
-conn.sendMessage(chat, { delete: m.quoted.vM.key })
-}
-handler.help = ['del', 'delete']
-handler.tags = ['tools']
-handler.command = /^del(ete)?$/i
-handler.group = true
-handler.admin = true
-export default handler*/
