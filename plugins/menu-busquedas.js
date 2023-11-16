be2913af679ebb519e3d77f9869d51858da50d8b
MD <122848691+WorkCwp@users.noreply.github.com>
2023-11-15 21:11:39 -0500

    ♰⍣𝑪𝒓𝒊𝒘𝒊𝒍𝒐𝒑𝑩𝒐𝒕-𝑴𝑫࿐⛦

--- plugins/menu-busquedas.js
+++ plugins/menu-busquedas.js
@@ -22,9 +22,7 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
     const doc = ['pdf', 'zip', 'vnd.openxmlformats-officedocument.presentationml.presentation', 'vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'vnd.openxmlformats-officedocument.wordprocessingml.document'];
     const document = doc[Math.floor(Math.random() * doc.length)];
     const str = `
-*┏┄┄⟞ *♰⍣𝑪𝒓𝒊𝒘𝒊𝒍𝒐𝒑𝑩𝒐𝒕-𝑴𝑫࿐⛦* ⟝┄┄┓*
-   
-*╭───── • ◆ • ─────┅┅┄┄*
+    *╭───── • ◆ • ─────┅┅┄┄*
  *< 𝔹𝕌𝕊ℂ𝔸𝔻𝕆ℝ𝔼𝕊 />*
  
 ▸🔍 _${usedPrefix}githubsearch *<texto>*_
