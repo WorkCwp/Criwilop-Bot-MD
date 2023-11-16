8e21151d3de6fb204d3db6130fe2a3d28407b9da
MD <122848691+WorkCwp@users.noreply.github.com>
2023-11-16 18:38:45 -0500

    Update menu-jadibot.js

--- plugins/menu-jadibot.js
+++ plugins/menu-jadibot.js
@@ -23,12 +23,13 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
     const document = doc[Math.floor(Math.random() * doc.length)];
     const str = `
 *╭───── • ◆ • ─────┅┅┄┄*
- *< 𝕊𝔼ℝ𝔹𝕆𝕋 - 𝕁𝔸𝔻𝕀𝔹𝕆𝕋 />*
+     *ᴍᴇɴᴜ ᴊᴀᴅɪʙᴏᴛ*
    
-▸🤖 _${usedPrefix}serbot --code_
-▸🤖 _${usedPrefix}serbot_
-▸🤖 _${usedPrefix}deletebot_
-*╰───── • ◆ • ─────┅┅┄┄*`.trim();
+  ▸🤖 _${usedPrefix}serbot --code_
+  ▸🤖 _${usedPrefix}serbot_
+  ▸🤖 _${usedPrefix}deletebot_
+*╰───── • ◆ • ─────┅┅┄┄*
+*_BY: ♰⍣𝑪𝒓𝒊𝒘𝒊𝒍𝒐𝒑𝑩𝒐𝒕-𝑴𝑫࿐⛦_*`.trim();
     if (m.isGroup) {
       // await conn.sendFile(m.chat, vn, 'menu.mp3', null, m, true, { type: 'audioMessage', ptt: true})
       const fkontak2 = {'key': {'participants': '0@s.whatsapp.net', 'remoteJid': 'status@broadcast', 'fromMe': false, 'id': 'Halo'}, 'message': {'contactMessage': {'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}, 'participant': '0@s.whatsapp.net'};
