cfa47cdcd071dd88bcc13228ca605df1ba81a7fd
MD <122848691+WorkCwp@users.noreply.github.com>
2023-11-16 18:27:36 -0500

    ♰⍣𝑪𝒓𝒊𝒘𝒊𝒍𝒐𝒑𝑩𝒐𝒕-𝑴𝑫࿐⛦

--- plugins/menu-descargas.js
+++ plugins/menu-descargas.js
@@ -23,7 +23,7 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
     const document = doc[Math.floor(Math.random() * doc.length)];
     const str = `
 *╭───── • ◆ • ─────┅┅┄┄*
-   *< 𝔻𝔼𝕊ℂ𝔸ℝ𝔾𝔸𝕊 />*
+      *ᴅᴇsᴄᴀʀɢᴀs*
    
 ▸🔰 _${usedPrefix}instagram *<enlace / link / url>*_
 ▸🔰 _${usedPrefix}mediafire *<enlace / link / url>*_
@@ -58,7 +58,8 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
 ▸🔰 _${usedPrefix}igstalk *<nombre de usuario>*_
 ▸🔰 _${usedPrefix}igstory *<nombre de usuario>*_
 ▸🔰 _${usedPrefix}tiktokstalk *<username>*_
-*╰───── • ◆ • ─────┅┅┄┄*`.trim();
+*╰───── • ◆ • ─────┅┅┄┄*
+*_BY: ♰⍣𝑪𝒓𝒊𝒘𝒊𝒍𝒐𝒑𝑩𝒐𝒕-𝑴𝑫࿐⛦_*`.trim();
     if (m.isGroup) {
       // await conn.sendFile(m.chat, vn, 'menu.mp3', null, m, true, { type: 'audioMessage', ptt: true})
       const fkontak2 = {'key': {'participants': '0@s.whatsapp.net', 'remoteJid': 'status@broadcast', 'fromMe': false, 'id': 'Halo'}, 'message': {'contactMessage': {'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}, 'participant': '0@s.whatsapp.net'};
