a0b50e9d6e07cdd51e40e10dc8174d29d22eb98e
MD <122848691+WorkCwp@users.noreply.github.com>
2023-11-20 23:00:09 -0500

    ♰⍣𝑪𝒓𝒊𝒘𝒊𝒍𝒐𝒑𝑩𝒐𝒕-𝑴𝑫࿐⛦

--- plugins/random-imgApi.js
+++ plugins/random-imgApi.js
@@ -1,11 +1,14 @@
 import fetch from 'node-fetch';
+
 const handler = async (m, {conn, usedPrefix, command}) => {
   const res = await fetch('https://api-brunosobrino.zipponodes.xyz/api/wallpaper/hacker');
   if (!res.ok) throw await res.text();
   const buffer = await res.buffer();
   conn.sendFile(m.chat, buffer, 'wallpaper.png', `texto prueba`, m);
 };
+
 handler.help = [''];
 handler.tags = [''];
 handler.command = /^(p)$/i;
+
 export default handler;
