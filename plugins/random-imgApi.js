cc0d57d953bb4f0bb9d8cdccf18ce7b83e21e3d4
MD <122848691+WorkCwp@users.noreply.github.com>
2023-11-20 22:49:34 -0500

    ♰⍣𝑪𝒓𝒊𝒘𝒊𝒍𝒐𝒑𝑩𝒐𝒕-𝑴𝑫࿐⛦

--- plugins/random-imgApi.js
+++ plugins/random-imgApi.js
@@ -4,7 +4,7 @@ const handler = async (m, {conn, usedPrefix, command}) => {
   if (!res.ok) throw await res.text();
   const json = await res.json();
   if (!json.url) throw 'Error!';
-  conn.sendFile(m.chat, json.url, 'error.jpg', `texto prueba`, m);
+  conn.sendFile(m.chat, img, 'error.jpg', `texto prueba`, m);
 // conn.sendButton(m.chat, `𝙰-𝙰𝚁𝙰 𝙰𝚁𝙰 𝚂𝙴𝙼𝙿𝙰𝙸~~`, author, json.url, [['🔄 𝚂𝙸𝙶𝚄𝙸𝙴𝙽𝚃𝙴 🔄', `/${command}`]], m)
 };
 handler.help = [''];
