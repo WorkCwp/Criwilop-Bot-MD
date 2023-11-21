42192084f9d7b349c85607651fca1743e8df8b92
MD <122848691+WorkCwp@users.noreply.github.com>
2023-11-20 22:54:37 -0500

    Update random-imgApi.js

--- plugins/random-imgApi.js
+++ plugins/random-imgApi.js
@@ -3,7 +3,7 @@ const handler = async (m, {conn, usedPrefix, command}) => {
   const res = await fetch('https://api-brunosobrino.zipponodes.xyz/api/wallpaper/wpgaming');
   if (!res.ok) throw await res.text();
   const buffer = await res.buffer();
-  conn.sendFile(m.chat, buffer, 'wallpaper.jpg', `¡Aquí tienes un wallpaper de gaming!`, m);
+  conn.sendFile(m.chat, buffer, 'wallpaper.png', `¡Aquí tienes un wallpaper de gaming!`, m);
 };
 handler.help = [''];
 handler.tags = [''];
