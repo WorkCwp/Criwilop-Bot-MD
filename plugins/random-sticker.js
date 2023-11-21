6fb617b7e6e11b5b6aa342c34d66ae3e2110092e
MD <122848691+WorkCwp@users.noreply.github.com>
2023-11-20 23:05:47 -0500

    ♰⍣𝑪𝒓𝒊𝒘𝒊𝒍𝒐𝒑𝑩𝒐𝒕-𝑴𝑫࿐⛦

--- plugins/random-sticker.js
+++ plugins/random-sticker.js
@@ -1,6 +1,7 @@
 import fetch from 'node-fetch';
 
 const handler = async (m, {conn, usedPrefix, command}) => {
+  m.react('🕶️')
   const res = await fetch('https://api-brunosobrino.zipponodes.xyz/api/wallpaper/hacker');
   if (!res.ok) throw await res.text();
   const buffer = await res.buffer();
