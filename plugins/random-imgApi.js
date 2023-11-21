02c6695b8d2135e32f7823a71b1e8cd2a10456a5
MD <122848691+WorkCwp@users.noreply.github.com>
2023-11-20 22:53:46 -0500

    ♰⍣𝑪𝒓𝒊𝒘𝒊𝒍𝒐𝒑𝑩𝒐𝒕-𝑴𝑫࿐⛦

--- plugins/random-imgApi.js
+++ plugins/random-imgApi.js
@@ -2,12 +2,10 @@ import fetch from 'node-fetch';
 const handler = async (m, {conn, usedPrefix, command}) => {
   const res = await fetch('https://api-brunosobrino.zipponodes.xyz/api/wallpaper/wpgaming');
   if (!res.ok) throw await res.text();
-  const json = await res.json();
-  if (!json.url) throw 'Error!';
-  conn.sendFile(m.chat, img, 'error.jpg', `texto prueba`, m);
-// conn.sendButton(m.chat, `𝙰-𝙰𝚁𝙰 𝙰𝚁𝙰 𝚂𝙴𝙼𝙿𝙰𝙸~~`, author, json.url, [['🔄 𝚂𝙸𝙶𝚄𝙸𝙴𝙽𝚃𝙴 🔄', `/${command}`]], m)
+  const buffer = await res.buffer();
+  conn.sendFile(m.chat, buffer, 'wallpaper.jpg', `¡Aquí tienes un wallpaper de gaming!`, m);
 };
 handler.help = [''];
 handler.tags = [''];
-handler.command = /^(prueba)$/i;
-export default handler
+handler.command = /^(p)$/i;
+export default handler;
