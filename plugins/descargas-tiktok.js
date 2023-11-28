010324061b2713f3361637459ae632c5524e3bae
MD <122848691+WorkCwp@users.noreply.github.com>
2023-11-28 11:14:17 -0500

    ♰⍣𝑪𝒓𝒊𝒘𝒊𝒍𝒐𝒑𝑩𝒐𝒕-𝑴𝑫࿐⛦

--- plugins/descargas-tiktok.js
+++ plugins/descargas-tiktok.js
@@ -1,13 +1,19 @@
 import fetch from 'node-fetch';
 const handler = async (m, {conn, text}) => {
   try {
-    const res = await fetch(`https://api-phx.zipponodes.xyz/api/tiktokv2?url=${text}`);
+    const url = text.split(' ')[1]; // Obtener el enlace de TikTok del mensaje
+    const res = await fetch(`https://api-phx.zipponodes.xyz/api/tiktokv2?url=${url}`);
+    const videoUrl = await res.text(); // Obtener la URL del video descargado
+
+    // Enviar el video a WhatsApp
+    await conn.sendFile(m.chat, videoUrl, 'video.mp4', 'Aquí está el video de TikTok que solicitaste');
+
   } catch {
-    throw '*Error!*';
+    throw '*¡Error!* No se pudo descargar el video de TikTok';
   }
 };
-handler.help = ['tiktok-dl'];
+handler.help = ['tt'];
 handler.tags = ['tiktok'];
-handler.command = /^tt$/i;
+handler.command = /^\tt/i;
 handler.fail = null;
 export default handler;
