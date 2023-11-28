17d3fb7cc93cfa636b9617c9f4081bdbc1d5ae5a
MD <122848691+WorkCwp@users.noreply.github.com>
2023-11-28 13:59:15 -0500

    ♰⍣𝑪𝒓𝒊𝒘𝒊𝒍𝒐𝒑𝑩𝒐𝒕-𝑴𝑫࿐⛦

--- plugins/aGame.js
+++ plugins/aGame.js
@@ -1,7 +1,7 @@
 // Importar las dependencias necesarias
-import { Client } = from ('@whiskeysockets/baileys');
-import { Message } = from ('@adiwajshing/keyed-db');
-import { generateGame } = from ('@green-code/music-track-data');
+import { Client } from ('@whiskeysockets/baileys');
+import { Message } from ('@adiwajshing/keyed-db');
+import { generateGame } from ('@green-code/music-track-data');
 
 // Crear un cliente de WhatsApp
 const client = new Client();
