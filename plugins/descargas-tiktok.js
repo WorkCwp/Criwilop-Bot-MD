55755a4f04c6980d5d210e4086cc18d03b9a07b0
MD <122848691+WorkCwp@users.noreply.github.com>
2023-11-28 11:07:25 -0500

    ♰⍣𝑪𝒓𝒊𝒘𝒊𝒍𝒐𝒑𝑩𝒐𝒕-𝑴𝑫࿐⛦

--- plugins/descargas-tiktok.js
+++ plugins/descargas-tiktok.js
@@ -1,1 +1,16 @@
-.
+import fetch from 'node-fetch';
+const handler = async (m, {conn, text}) => {
+  try {
+    const res = await fetch('https://api.thedogapi.com/v1/images/search');
+    const img = await res.json();
+    const caption = `_♰⍣𝑪𝒓𝒊𝒘𝒊𝒍𝒐𝒑𝑩𝒐𝒕-𝑴𝑫࿐⛦_`.trim();
+    conn.sendFile(m.chat, img[0].url, 'dog.jpg', caption, m);
+  } catch {
+    throw '*Error!*';
+  }
+};
+handler.help = ['tiktok-dl'];
+handler.tags = ['tiktok'];
+handler.command = /^tt$/i;
+handler.fail = null;
+export default handler;
