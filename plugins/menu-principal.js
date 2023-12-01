8348f27a8de3b1d040a54671788386939be93742
MD <122848691+WorkCwp@users.noreply.github.com>
2023-12-01 07:58:14 -0500

    ♰⍣𝑪𝒓𝒊𝒘𝒊𝒍𝒐𝒑𝑩𝒐𝒕-𝑴𝑫࿐⛦

--- plugins/menu-principal.js
+++ plugins/menu-principal.js
@@ -29,7 +29,7 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
    
              *🎮 Nivel:* ${level}
              *🧰 Experiencia:* ${exp}
-⇙⇙⇙⇙⇙⇙⇙⇙
+  ⇙⇙⇙⇙
 ${readMore}
 *╭───── • ◆ • ─────┅┅┄┄*
    🪢 *ᴇsᴛᴀᴅᴏ ᴅᴇʟ ʙᴏᴛ* 🪢
