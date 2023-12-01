24f1bf0dd70664d0ea0d3e1aa6eae271400d58f1
MD <122848691+WorkCwp@users.noreply.github.com>
2023-12-01 07:50:33 -0500

    ♰⍣𝑪𝒓𝒊𝒘𝒊𝒍𝒐𝒑𝑩𝒐𝒕-𝑴𝑫࿐⛦

--- plugins/menu-principal.js
+++ plugins/menu-principal.js
@@ -32,7 +32,7 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
 
 ${readMore}
 *╭───── • ◆ • ─────┅┅┄┄*
-     *ᴇsᴛᴀᴅᴏ ᴅᴇʟ ʙᴏᴛ*
+   🪢 *ᴇsᴛᴀᴅᴏ ᴅᴇʟ ʙᴏᴛ* 🪢
    
  ${(conn.user.jid == global.conn.user.jid ? '' : `Jadibot de: https://wa.me/${global.conn.user.jid.split`@`[0]}`) || '*Este es el Bot oficial*'}
 
