5274653d7965b80564d112f3ac1a19877fe3cdcd
MD <122848691+WorkCwp@users.noreply.github.com>
2023-12-01 21:30:17 -0500

    ♰⍣𝑪𝒓𝒊𝒘𝒊𝒍𝒐𝒑𝑩𝒐𝒕-𝑴𝑫࿐⛦

--- plugins/downloader-play_v2.js
+++ plugins/downloader-play_v2.js
@@ -5,10 +5,12 @@ let mimeType;
 let fileName;
 let apiUrl;
 let enviando = false;
+
 const handler = async (m, { command, usedPrefix, conn, text }) => {
-  if (!text) throw `*⚠️ Nombre de la canción/video faltante, por favor ingrese el comando más el nombre, título o link de alguna canción o video de YouTube.*\n\n*—◉ Ejemplo 1:\n *${usedPrefix + command} Kevin kaarl colapso*`;
-if (enviando) return;
-    enviando = true
+  if (!text) throw `*⚠️ Falta el nombre de la canción/video. Por favor, ingrese el comando seguido del nombre, título o enlace de una canción o video de YouTube.*\n\n*—◉ Ejemplo 1:\n *${usedPrefix + command} Kevin kaarl colapso*`;
+  if (enviando) return;
+  enviando = true;
+
   try {
     const apiUrls = [
       `https://api-brunosobrino.zipponodes.xyz/api/ytplay?text=${text}`
@@ -30,56 +32,58 @@ if (enviando) return;
     } else {
       try {
         if (command === 'play') {
-              m.react('🎧')
-              apiUrl = `https://api-brunosobrino.zipponodes.xyz/api/v1/ytmp3?url=${data.resultado.url}`;
-              mimeType = 'audio/mpeg';
-              fileName = 'error.mp3';
-              buff = await conn.getFile(apiUrl);
-            } else if (command === 'play2') {
-          m.react('🎥')
-              apiUrl = `https://api-brunosobrino.zipponodes.xyz/api/v1/ytmp4?url=${data.resultado.url}`;
-              mimeType = 'video/mp4';
-              fileName = 'error.mp4';
-              buff = await conn.getFile(apiUrl);
+          m.react('🎧');
+          apiUrl = `https://api-brunosobrino.zipponodes.xyz/api/v1/ytmp3?url=${data.resultado.url}`;
+          mimeType = 'audio/mpeg';
+          fileName = 'error.mp3';
+          buff = await fetch(apiUrl).then(res => res.buffer());
+        } else if (command === 'play2') {
+          m.react('🎥');
+          apiUrl = `https://api-brunosobrino.zipponodes.xyz/api/v1/ytmp4?url=${data.resultado.url}`;
+          mimeType = 'video/mp4';
+          fileName = 'error.mp4';
+          buff = await fetch(apiUrl).then(res => res.buffer());
         }
       } catch {
-          try {
-            if (command === 'play') {
-              m.react('🎧')
-              apiUrl = `https://api-brunosobrino.onrender.com/api/v1/ytmp3?url=${data.resultado.url}`;
-              mimeType = 'audio/mpeg';
-              fileName = 'error.mp3';
-              buff = await conn.getFile(apiUrl);
-            } else if (command === 'play2') {
-              m.react('🎥')
-              apiUrl = `https://api-brunosobrino.onrender.com/api/v1/ytmp4?url=${data.resultado.url}`;
-              mimeType = 'video/mp4';
-              fileName = 'error.mp4';
-              buff = await conn.getFile(apiUrl);
-           }
-          } catch {
-            enviando = false;
-            throw `*⚠️ Error al descargar el video/canción desde las APIs disponibles.`;
+        try {
+          if (command === 'play') {
+            m.react('🎧');
+            apiUrl = `https://api-brunosobrino.onrender.com/api/v1/ytmp3?url=${data.resultado.url}`;
+            mimeType = 'audio/mpeg';
+            fileName = 'error.mp3';
+            buff = await fetch(apiUrl).then(res => res.buffer());
+          } else if (command === 'play2') {
+            m.react('🎥');
+            apiUrl = `https://api-brunosobrino.onrender.com/api/v1/ytmp4?url=${data.resultado.url}`;
+            mimeType = 'video/mp4';
+            fileName = 'error.mp4';
+            buff = await fetch(apiUrl).then(res => res.buffer());
           }
-       }
-    }m.react('☑️')
+        } catch {
+          enviando = false;
+          throw `*⚠️ Error al descargar el video/canción desde las APIs disponibles.*`;
+        }
+      }
+    }
 
-    const dataMessage = `*⍣ 🔗 Titulo🍁:* *${data.resultado.title}*
+    m.react('☑️');
 
-${data.resultado.url}`;
+    const dataMessage = `*⍣ 🔗 Título🍁:* *${data.resultado.title}*\n\n${data.resultado.url}`;
     await conn.sendMessage(m.chat, { text: dataMessage }, { quoted: m });
-    
+
     if (buff) {
-      await conn.sendMessage(m.chat, {[mimeType.startsWith('audio') ? 'audio' : 'video']: buff.data, mimetype: mimeType, fileName: fileName}, {quoted: m});
+      await conn.sendMessage(m.chat, { [mimeType.startsWith('audio') ? 'audio' : 'video']: buff, mimetype: mimeType, fileName: fileName }, { quoted: m });
       enviando = false;
     } else {
       enviando = false;
-      throw `*⚠️ Error al descargar el video/canción desde las APIs disponibles.`;
+      throw `*⚠️ Error al descargar el video/canción desde las APIs disponibles.*`;
     }
   } catch (error) {
     enviando = false;
     throw `*⚠️ Error: ${error.message || 'Ocurrió un error inesperado'}.*`;
   }
 };
+
 handler.command = ['play', 'play2'];
+
 export default handler;
