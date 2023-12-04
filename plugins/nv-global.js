923f365bb686bdab5ba964e7ab47e8ea52d306cb
MD <122848691+WorkCwp@users.noreply.github.com>
2023-12-04 18:09:26 -0500

    ♰⍣𝑪𝒓𝒊𝒘𝒊𝒍𝒐𝒑𝑩𝒐𝒕-𝑴𝑫࿐⛦

--- plugins/nv-global.js
+++ plugins/nv-global.js
@@ -99,7 +99,7 @@ if (/^adios$/i.test(m.text) && !chat.isBanned) {
 if (/^grupo muerto$/i.test(m.text) && !chat.isBanned) {
     if (!db.data.chats[m.chat].audios) return;
     if (!db.data.settings[this.user.jid].audios_bot && !m.isGroup) return;
-    const vn = './media/audios/motivacion.mp3';
+    const vn = './media/audios/Motivacion.mp3';
     mconn.conn.sendPresenceUpdate('recording', m.chat);
     mconn.conn.sendMessage(m.chat, {audio: {url: vn}, fileName: 'error.mp3', mimetype: 'audio/mpeg', ptt: true}, {quoted: m});
 }
