0b6d7cd59823f014aeb8f9b3c2da6769c612fa23
MD <122848691+WorkCwp@users.noreply.github.com>
2023-12-04 18:20:41 -0500

    ♰⍣𝑪𝒓𝒊𝒘𝒊𝒍𝒐𝒑𝑩𝒐𝒕-𝑴𝑫࿐⛦

--- plugins/menu-audios.js
+++ plugins/menu-audios.js
@@ -20,8 +20,7 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
     const doc = ['pdf', 'zip', 'vnd.openxmlformats-officedocument.presentationml.presentation', 'vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'vnd.openxmlformats-officedocument.wordprocessingml.document'];
     const document = doc[Math.floor(Math.random() * doc.length)];
     const str = `*╭───── • ◆ • ─────┅┅┄┄*
-     * ${taguser}*
-     *ᴍᴇɴᴜ ᴅᴇ ᴀᴜᴅɪᴏs *
+     *ᴍᴇɴᴜ ᴅᴇ ᴀᴜᴅɪᴏs*
 
   🔸 *_Basado_*
   🔸 *_bienvenido_*
