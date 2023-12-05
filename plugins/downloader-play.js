db92a921d6e9872d3b323814c1974d3a7c570ea5
MD <122848691+WorkCwp@users.noreply.github.com>
2023-12-05 16:36:44 -0500

    ♰⍣𝑪𝒓𝒊𝒘𝒊𝒍𝒐𝒑𝑩𝒐𝒕-𝑴𝑫࿐⛦

---
+++ plugins/downloader-play.js
@@ -0,0 +1,26 @@
+import yts from 'yt-search'
+let handler = async (m, { conn, command, text, usedPrefix }) => {
+	
+	if (!text) throw `⚠️ Ingresa el título de una canción`
+	let res = await yts(text)
+	let vid = res.videos[0]
+	if (!vid) throw `🔸 Vídeo/Audio no encontrado`
+	let { title, description, thumbnail, videoId, timestamp, views, ago, url } = vid
+	//const url = 'https://www.youtube.com/watch?v=' + videoId
+	m.react('🎧')
+	let play = `
+      *MUSIC*
+
+ 🔸 *Título* : ${title}
+ 🔸 *Publicado:* ${ago}
+ 🔸 *Duración:* ${timestamp}
+ 🔸 *Vistas:* ${views}`
+ await conn.sendButton(m.chat, play, fgig, thumbnail, [
+    ['🎶 MP3', `${usedPrefix}fgmp3 ${url}`],
+    ['🎥 MP4', `${usedPrefix}fgmp4 ${url}`]
+  ], m, rpl)
+}
+handler.help = ['play']
+handler.tags = ['dl']
+handler.command = ['play', 'playvid']
+handler.disabled = true
