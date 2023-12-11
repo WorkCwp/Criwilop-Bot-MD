e55eaf5ccc9bd14abfbe9fa102724bc8141f5dc2
MD <122848691+WorkCwp@users.noreply.github.com>
2023-12-11 08:05:05 -0500

    ♰⍣𝑪𝒓𝒊𝒘𝒊𝒍𝒐𝒑𝑩𝒐𝒕-𝑴𝑫࿐⛦

--- plugins/downloader-playlist.js
+++ plugins/downloader-playlist.js
@@ -26,10 +26,10 @@ const handler = async (m, {conn, text, usedPrefix, command}) => {
       vids_.urls.push(link);
       return `[${i + 1}] ${v.title}
 ↳ 🫐 *_Link :_* ${v.url}
-↳ 🕒 *_Duración :_* ${v.timestamp};
+↳ 🕒 *_Duración :_* ${v.timestamp}`;
     }).join('\n\n─┈┈┈┄┄╌╌╌╌┄┄┈┈┈─\n\n');
     conn.sendFile(m.chat, results.all[0].thumbnail, 'yts.jpeg', textoInfo + '\n\n' + teks, m);
-    global.videoList.push(vids_);`
+    global.videoList.push(vids_);
   } catch {
     await m.reply('*⚠️ 𝙴𝚁𝚁𝙾𝚁, 𝙿𝙾𝚁 𝙵𝙰𝚅𝙾𝚁 𝚅𝚄𝙴𝙻𝚅𝙰 𝙰 𝙸𝙽𝚃𝙴𝙽𝚃𝙰𝚁𝙻𝙾 𝙲𝙾𝙽 𝙾𝚃𝚁𝙾 𝙽𝙾𝙼𝙱𝚁𝙴 𝙳𝙴 𝚄𝙽𝙰 𝙲𝙰𝙽𝙲𝙸𝙾𝙽*');
   }
