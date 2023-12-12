077157278e7a68daa26c640a61e030560da92fba
MD <122848691+WorkCwp@users.noreply.github.com>
2023-12-11 20:22:34 -0500

    ♰⍣𝑪𝒓𝒊𝒘𝒊𝒍𝒐𝒑𝑩𝒐𝒕-𝑴𝑫࿐⛦

--- plugins/rpg-leaderboard.js
+++ plugins/rpg-leaderboard.js
@@ -24,22 +24,22 @@ const handler = async (m, {conn, args, participants}) => {
 *╭───────────⊱◈◈◈⊰────────┅┅┄┄*
         *♰⍣𝑪𝒓𝒊𝒘𝒊𝒍𝒐𝒑𝑩𝒐𝒕-𝑴𝑫࿐⛦*
     
-—◉ *TOP ${len} EXP 🌟*
-*👤 Tú posición:* ${usersExp.indexOf(m.sender) + 1} de ${usersExp.length}
+🔸 *TOP ${len} EXP 🕶️*
+*🍂 Tú posición:* ${usersExp.indexOf(m.sender) + 1} de ${usersExp.length}
 
 ${sortedExp.slice(0, len).map(({jid, exp}, i) => `${i + 1}. ${participants.some((p) => jid === p.jid) ? `(${conn.getName(jid)}) wa.me/` : '@'}${jid.split`@`[0]} *${exp} exp*`).join`\n`}
 
-—◉ *TOP ${len} DIAMANTES 💎*
-*👤 Tú posición:* ${usersLim.indexOf(m.sender) + 1} de ${usersLim.length}
+🔸 *TOP ${len} DIAMANTES ❄️*
+*🌀 Tú posición:* ${usersLim.indexOf(m.sender) + 1} de ${usersLim.length}
 
 ${sortedLim.slice(0, len).map(({jid, limit}, i) => `${i + 1}. ${participants.some((p) => jid === p.jid) ? `(${conn.getName(jid)}) wa.me/` : '@'}${jid.split`@`[0]} *${limit} diamantes*`).join`\n`}
 
-—◉ *TOP ${len} NIVEL 🎚️*
-*👤 Tú posición:* ${usersLevel.indexOf(m.sender) + 1} de ${usersLevel.length}
+🔸 *TOP ${len} NIVEL 🗡️*
+*🪵 Tú posición:* ${usersLevel.indexOf(m.sender) + 1} de ${usersLevel.length}
 
 ${sortedLevel.slice(0, len).map(({jid, level}, i) => `${i + 1}. ${participants.some((p) => jid === p.jid) ? `(${conn.getName(jid)}) wa.me/` : '@'}${jid.split`@`[0]} *nivel ${level}*`).join`\n`}
 
-*⚔️ ${randomAdventurePhrase} ⚔️*
+*🍷 ${randomAdventurePhrase} 🍷*
 *╰───────────⊱◈◈◈⊰────────┅┅┄┄*`.trim();
   conn.sendMessage(m.chat, {text: texto, mentions: conn.parseMention(texto)}, {quoted: m})
 };
