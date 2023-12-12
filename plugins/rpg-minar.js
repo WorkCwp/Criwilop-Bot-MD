c25a27b49fc62934c31866f33d0eb78370d444cc
MD <122848691+WorkCwp@users.noreply.github.com>
2023-12-11 20:30:45 -0500

    ♰⍣𝑪𝒓𝒊𝒘𝒊𝒍𝒐𝒑𝑩𝒐𝒕-𝑴𝑫࿐⛦

--- plugins/rpg-minar.js
+++ plugins/rpg-minar.js
@@ -1,8 +1,8 @@
 const handler = async (m, {conn, isPrems}) => {
-  const hasil = Math.floor(Math.random() * 1000);
-  const time = global.db.data.users[m.sender].lastmiming + 600000;
-  if (new Date - global.db.data.users[m.sender].lastmiming < 600000) throw `*[ ⏲️ ] 𝙴𝚜𝚙𝚎𝚛𝚊 ${msToTime(time - new Date())} 𝚙𝚊𝚛𝚊 𝚟𝚘𝚕𝚟𝚎𝚛 𝚊 𝚖𝚒𝚗𝚊𝚛*`;
-  m.reply(`*[ 🎉 ] 𝙶𝚎𝚗𝚒𝚊𝚕, 𝚖𝚒𝚗𝚊𝚜𝚝𝚎 ${hasil} 𝚇𝙿*`);
+  const hasil = Math.floor(Math.random() * 9999);
+  const time = global.db.data.users[m.sender].lastmiming + 900000;
+  if (new Date - global.db.data.users[m.sender].lastmiming < 900000) throw `*🪦 𝙴𝚜𝚙𝚎𝚛𝚊 ${msToTime(time - new Date())} 𝚙𝚊𝚛𝚊 𝚟𝚘𝚕𝚟𝚎𝚛 𝚊 𝚖𝚒𝚗𝚊𝚛*`;
+  m.reply(`*🌀 𝚖𝚒𝚗𝚊𝚜𝚝𝚎 🌬️${hasil} 𝚇𝙿*`);
   global.db.data.users[m.sender].lastmiming = new Date * 1;
 };
 handler.help = ['minar'];
