2e7d5e09e67245cf30bc30cd1e36cfecf8431ecb
MD <122848691+WorkCwp@users.noreply.github.com>
2023-12-11 20:45:46 -0500

    ♰⍣𝑪𝒓𝒊𝒘𝒊𝒍𝒐𝒑𝑩𝒐𝒕-𝑴𝑫࿐⛦

--- plugins/rpg-shop.js
+++ plugins/rpg-shop.js
@@ -7,11 +7,11 @@ const handler = async (m, {conn, command, args}) => {
     global.db.data.users[m.sender].exp -= xpperlimit * count;
     global.db.data.users[m.sender].limit += count;
     conn.reply(m.chat, `
-┌─「 *NOTA DE PAGO* 」
-‣ *Compra nominal* : + ${count}💎 
-‣ *Gastado* : -${xpperlimit * count} XP
-└──────────────`, m);
-  } else conn.reply(m.chat, `❎ Lo siento, no tienes suficientes *XP* para comprar *${count}* Diamantes💎`, m);
+*╭───── PAGO TOTAL ───┅┅┄┄*
+ 🔸 *Compra Total* : + ${count}💎 
+ 🔸 *Gastado* : -${xpperlimit * count} XP
+*╰───── • ◆ • ─────┅┅┄┄* `, m);
+  } else conn.reply(m.chat, `❎ Lo siento, no tienes suficientes *XP* para comprar *${count}* Diamantes 💎`, m);
 };
 handler.help = ['Buy', 'Buyall'];
 handler.tags = ['xp'];
