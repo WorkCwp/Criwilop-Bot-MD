c5b5c7356a1a4aefcdfa0baf4e7cee34c3d19ebf
MD <122848691+WorkCwp@users.noreply.github.com>
2023-12-26 19:30:24 -0500

    ♰⍣𝑪𝒓𝒊𝒘𝒊𝒍𝒐𝒑𝑩𝒐𝒕-𝑴𝑫࿐⛦

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -27,7 +27,7 @@ const text = `*╭───── • ◆ • ─────┅┅┄┄*
         'mediaUrl': 'https://github.com/WorkCwp/Criwilop-Bot-MD',
         'mediaType': 2,
         'previewType': 'pdf',
-        'title': '😗👉🍡',
+        'title': '♬ ♬ ♬',
         'body': wm,
         'thumbnail': imagen1,
         'sourceUrl': 'https://m.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUJcmljayByb2xs'}},
