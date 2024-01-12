27463e8b7d84438049d9a220dfeea069e87eac85
MD <122848691+WorkCwp@users.noreply.github.com>
2024-01-11 21:25:35 -0500

    ♰⍣𝑪𝒓𝒊𝒘𝒊𝒍𝒐𝒑𝑩𝒐𝒕-𝑴𝑫࿐⛦

--- plugins/owner-anadirXP.js
+++ plugins/owner-anadirXP.js
@@ -7,7 +7,7 @@ const handler = async (m, {conn, text}) => {
   if (!who) throw '*⚠️ 𝙴𝚃𝙸𝚀𝚄𝙴𝚃𝙰 𝙰 𝚄𝙽 𝚄𝚂𝚄𝙰𝚁𝙸𝙾 𝙲𝙾𝙽 𝙴𝙻 @𝚝𝚊𝚐*';
   const txt = text.replace('@' + who.split`@`[0], '').trim();
   if (!txt) throw '*⚠️ 𝙸𝙽𝙶𝚁𝙴𝚂𝙰 𝙻𝙰 𝙲𝙰𝙽𝚃𝙸𝙳𝙰𝙳 𝙳𝙴 𝙴𝚇𝙿𝙴𝚁𝙸𝙴𝙽𝙲𝙸𝙰 (𝚇𝙿) 𝚀𝚄𝙴 𝙳𝙴𝚂𝙴𝙰 𝙰𝙽̃𝙰𝙳𝙸𝚁*';
-  if (isNaN(txt)) throw '*⚠️ 𝚂𝙸𝙼𝙱𝙾𝙻𝙾 𝙽𝙾 𝙰𝙳𝙼𝙸𝚃𝙸𝙳𝙾, 𝚂𝙾𝙻𝙾 𝙽𝚄𝙼𝙴𝚁𝙾𝚂!*';
+  if (isNaN(txt)) throw '*⚠️ 𝚂𝙾𝙻𝙾 𝙽𝚄𝙼𝙴𝚁𝙾𝚂!*';
   const xp = parseInt(txt);
   let exp = xp;
   const pjk = Math.ceil(xp * pajak);
@@ -20,6 +20,6 @@ const handler = async (m, {conn, text}) => {
 ▢  *𝚃𝚘𝚝𝚊𝚕:* ${xp}
 └──────────────`);
 };
-handler.command = ['añadirxp', 'addexp'];
-handler.owner = true;
+handler.command = ['añadirxp', 'addxp'];
+handler.rowner = true;
 export default handler;
